26d1db1448549c88c06a76c02c50156c67668e42
vizielod <elod.vizi@yahoo.com>
2019-05-20 01:37:12 +0200

    testIsNoun() and testIsNotNoun() methods implemented for WordTypeDetectorTest class

--- src/test/java/org/aksw/sparql2nl/naturallanguagegeneration/WordTypeDetectorTest.java
+++ src/test/java/org/aksw/sparql2nl/naturallanguagegeneration/WordTypeDetectorTest.java
@@ -1,4 +1,20 @@
 package org.aksw.sparql2nl.naturallanguagegeneration;
 
+import org.junit.Test;
+
 public class WordTypeDetectorTest {
+
+    private WordTypeDetector wtd = new WordTypeDetector();
+
+    @Test
+    public void testIsNoun(){
+        String testNoun = "Table";
+        assert(wtd.isNoun(testNoun));
+    }
+
+    @Test
+    public void testIsNotNoun(){
+        String testVerb = "To work";
+        assert(!wtd.isNoun(testVerb));
+    }
 }
